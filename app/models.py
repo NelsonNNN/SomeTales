9b9544fd69606549c4ad82a88e4c1f879a68841c
NelsonNNN <nelson.nyabuto51@gmail.com>
2020-06-26 18:59:03 +0300

    changed database

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(20), unique=True, nullable=False)
     image = db.Column(db.String(20), nullable=False, default='image.jpg')
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(65), nullable=False)
     posts = db.relationship('Post', backref='author', lazy= True)
 
     def get_reset_token(self, expires_sec = 1800):
