1b108c2a801c9c3cf6704ce09fa8bd2ad3f029bf
Nelson Nyamwaro <nelson.nyabuto51@gmail.com>
2023-03-20 07:46:04 +0300

    Second commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
